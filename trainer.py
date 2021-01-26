d093a6070a6e98b21493bd6102f1f9d5b95d703b
hanif <dev.har07@gmail.com>
2021-01-26 11:24:42 +0700

    add kfac preconditioner

--- trainer.py
+++ trainer.py
@@ -5,12 +5,17 @@ import torch.nn.functional as F
 import lib.dataset
 import lib.model
 import lib.evaluation
+import lib.ekfac_precond
+import lib.kfac_precond
 import argparse
 import numpy as np
 import time
 
 default_seed = 1
 default_epochs = 10
+default_lr = 0.9
+default_decay = 0.01
+default_epsilon = 0.01
 parser = argparse.ArgumentParser(
                     description="Trains and saves neural network for "
                                 "MNIST classification.")
@@ -22,16 +27,20 @@ parser.add_argument("-e", "--epochs",
                     default=default_epochs)
 parser.add_argument("-lr", "--learning_rate",
                     help="learning rate",
-                    default=default_epochs)
-parser.add_argument("-decay", "--decay",
+                    default=default_lr)
+parser.add_argument("-d", "--decay",
                     help="weight decay",
-                    default=default_epochs)
+                    default=default_decay)
+parser.add_argument("-eps", "--epsilon",
+                    help="Tikhonov regularization",
+                    default=default_epsilon)
 
 args = parser.parse_args()
 seed = int(args.seed)
 epochs = int(args.epochs)
 learning_rate = float(args.learning_rate)
 decay = float(args.decay)
+epsilon = float(args.epsilon)
 
 torch.cuda.set_device(0)
 torch.manual_seed(seed)
@@ -40,7 +49,10 @@ model = lib.model.MnistModel()
 train_loader, test_loader = lib.dataset.make_datasets()
 model = model.cuda()
 
-optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=decay)
+# optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=decay)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay=decay)
+# precond = lib.ekfac_precond.EKFAC(model, epsilon, alpha=1.0)
+precond = lib.kfac_precond.KFAC(model, epsilon)
 
 for epoch in range(epochs):
     t0 = time.time()
@@ -52,6 +64,7 @@ for epoch in range(epochs):
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
+        precond.step()
         optimizer.step()
 
         prediction = output.data.max(1)[1]   # first column has actual prob.
