77a6f465a8a0518983f30645b30ed28e91243ea8
hanif <dev.har07@gmail.com>
2021-01-31 12:02:09 +0700

    wip: sgld hyperparams tuning (5/n)

--- tune_sgld.py
+++ tune_sgld.py
@@ -8,22 +8,29 @@ import lib.model
 import lib.evaluation
 import lib.sgld as sgld
 import argparse
+import datetime
 
 import optuna
 from optuna.visualization import plot_optimization_history
 
-default_study = "sgld-study"
+default_trial = 50
 
 parser = argparse.ArgumentParser(
                     description="Perform  hyperparameter tuning of SGLD optimizer"
                                 "for MNIST classification.")
 parser.add_argument("-s", "--study",
                     help="hyperparam optimization study name. determine database file "+
-                         "to save to.",
-                    default=default_study)
+                         "to save to.")
+parser.add_argument("-t", "--trials",
+                    help="number of trials to perform",
+                    default=default_trial)
 
 args = parser.parse_args()
-study_name = str(args.study)
+if args.study:
+    study_name = str(args.study)
+else:
+    study_name = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+trials = int(args.trials)
 
 def train(model, optimizer, train_loader, test_loader, epochs):
     for _ in range(epochs):
@@ -55,16 +62,14 @@ def objective(trial):
     model = model.cuda()
 
     # hyperparams search space
-    lr = trial.suggest_loguniform("lr", 1e-5, 1e-1)
-    burn_in = trial.suggest_int("num_burn_in_steps", 10, 1000, step=30)
-    optimizer = sgld.SGLD(model.parameters(), lr=lr, num_burn_in_steps=burn_in)
+    optimizer = sgld_optimizer(model.parameters(), trial)
 
     accuracy = train(model, optimizer, train_loader, test_loader, epochs)
     return accuracy
 
 def print_stats(study):
-    pruned_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.PRUNED]
-    complete_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.COMPLETE]
+    pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
+    complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
 
     print("Study statistics: ")
     print("  Number of finished trials: ", len(study.trials))
@@ -80,12 +85,19 @@ def print_stats(study):
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
+def sgld_optimizer(params, trial):
+    # hyperparams search space
+    lr = trial.suggest_loguniform("lr", 1e-5, 1e-1)
+    burn_in = trial.suggest_int("num_burn_in_steps", 10, 1000, step=30)
+    optimizer = sgld.SGLD(params, lr=lr, num_burn_in_steps=burn_in)
+    return optimizer
+
 def main():
     # Add stream handler of stdout to show the messages
     optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
     storage_name = "sqlite:///{}.db".format(study_name)
     study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True, direction="maximize")
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=trials)
 
     print_stats(study)
 
