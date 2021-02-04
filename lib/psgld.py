d378fa4680a2227b70e0b11e0df3ff422da1fb51
hanif <dev.har07@gmail.com>
2021-02-04 08:34:18 +0700

    wip: psgld based on li chunyuan's impl.

--- lib/psgld.py
+++ lib/psgld.py
@@ -2,16 +2,17 @@ import torch
 from torch.distributions import Normal
 from torch.optim.optimizer import Optimizer, required
 
-class SGLD(Optimizer):
+class pSGLD(Optimizer):
     """
-    Translated from Li Chunyuan's implementation (matlab), with modification to support burn in steps
-    https://github.com/ChunyuanLI/pSGLD/blob/master/pSGLD_DNN/algorithms/SGLD.m
+    Translated from official implementation (matlab), with modification to support burn in steps
+    https://github.com/ChunyuanLI/pSGLD/blob/master/pSGLD_DNN/algorithms/SGLD_RMSprop.m
     """
 
-    def __init__(self, params, lr=required, train_size=required, weight_decay=0, lr_decay=0, num_burn_in_steps=300):
-        defaults = dict(lr=lr, train_size=train_size, weight_decay=weight_decay, lr_decay=lr_decay, 
-                        num_burn_in_steps=num_burn_in_steps)
-        super(SGLD, self).__init__(params, defaults)
+    def __init__(self, params, lr=required, train_size=required, rmsprop_decay=.99, eps=1e-1, 
+                    weight_decay=0, lr_offset=0, lr_decay=0, num_burn_in_steps=300):
+        defaults = dict(lr=lr, train_size=train_size, rmsprop_decay=rmsprop_decay, eps=eps, weight_decay=weight_decay, 
+                        lr_offset=lr_offset, lr_decay=lr_decay, num_burn_in_steps=num_burn_in_steps)
+        super(pSGLD, self).__init__(params, defaults)
 
     def step(self, lr=None):
         """
@@ -30,7 +31,7 @@ class SGLD(Optimizer):
                 if len(state) == 0:
                     state["iteration"] = 0
                 state["iteration"] += 1
-
+                
                 iteration = state["iteration"]
                 d_p = p.grad.data
                 if iteration <= group["num_burn_in_steps"]:
@@ -38,21 +39,31 @@ class SGLD(Optimizer):
                     p.data.add_(d_p, alpha=-group['lr'])
                     return loss
 
-                # if passed burn in steps, do Langevin-like SGD
+                # if passed burn in steps, do Langevin-like SGD with RMSProp preconditioner
                 weight_decay = group["weight_decay"]
                 if weight_decay > 0:
                     d_p.add_(p, alpha=weight_decay)
 
+                lr_offset = group["lr_offset"]
                 lr_decay = group["lr_decay"]
-                if lr_decay > 0:
+                if lr_offset > 0:
+                    lr * ((iteration+lr_offset) ** -lr_decay)
+                elif lr_decay > 0:
                     lr = lr * (iteration ** -lr_decay)
+
+                if not state["history"]:
+                    state["history"] = d_p ** 2
+
+                rmsd = group["rmsprop_decay"]
+                state["history"] = rmsd * state["history"] + (1-rmsd) * (d_p ** 2)
+                precond = (torch.tensor(eps) + torch.sqrt(d_p))
 
                 size = d_p.size()
                 langevin_noise = Normal(
                     torch.zeros(size),
                     torch.ones(size)
                 )
-                d_p = torch.tensor(lr)*d_p + torch.sqrt(2*torch.tensor(lr)) * \
+                d_p = torch.tensor(lr)*d_p / precond + torch.sqrt(2*torch.tensor(lr)/precond) * \
                         langevin_noise.sample().cuda()/group["train_size"]
                 p.data.add_(-d_p)
 
