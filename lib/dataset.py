62c972c2afddcdb9a690d6a7a465ac083da93dd6
hanif <dev.har07@gmail.com>
2021-03-07 17:02:47 +0700

    workaround for error downloading MNIST dataset

--- lib/dataset.py
+++ lib/dataset.py
@@ -2,6 +2,13 @@ import torch.utils
 from torchvision import datasets, transforms
 from .preproc import NoiseTransform
 
+# workaround for MNIST download problem: https://github.com/pytorch/vision/issues/1938#issuecomment-789986996
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+# end workaround
+
 def make_datasets(bs=50, test_bs=4096, noise=0):
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('data',
