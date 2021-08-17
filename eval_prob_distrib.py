c3dd1e85ac0c1ecc31a8a6d612f672fae19f78b6
hanif <dev.har07@gmail.com>
2021-08-17 15:55:24 +0700

    train & evaluate plots sgld multi-model; fix lr decay

--- eval_prob_distrib.py
+++ eval_prob_distrib.py
@@ -1,3 +1,5 @@
+# adapted from: https://github.com/fregu856/evaluating_bdl/blob/master/toyClassification/SGLD-64/eval_plots.py
+
 from lib.model import ToyNet
 
 import torch
@@ -11,25 +13,26 @@ import matplotlib.pyplot as plt
 
 import argparse
 
-# base_dir = "/content/sgld-experiments"
-# optimizer_name = "psgld2.pSGLD"
-
-# num_epochs = 50
+# example command to run:
+# !python eval_prob_distrib.py -b /content/sgld-experiments -id pSGLD-1600 -n 6
 
 parser = argparse.ArgumentParser(
                     description="Evaluate probability distribution plots "
                                 "From model trained using SGLD.")
 parser.add_argument("-b", "--base_dir",
                     help="base directory of the project")
-parser.add_argument("-o", "--optimizer",
-                    help="optimizer code name")
+parser.add_argument("-id", "--model_id",
+                    help="model id to identify checkpoint directory path")
 parser.add_argument("-e", "--epoch",
-                    help="number of epochs done for training the model")
+                    help="number of epochs done for training the model")                    
+parser.add_argument("-n", "--n_models",
+                    help="number of models trained")
 
 args = parser.parse_args()
 base_dir = str(args.base_dir)
-optimizer_name = str(args.optimizer_name)
+model_id = str(args.model_id)
 num_epochs = int(args.epoch)
+n_models = int(args.n_models)
 
 num_epochs_low = int(0.75*num_epochs)
 print (num_epochs_low)
@@ -38,54 +41,55 @@ x_min = -6.0
 x_max = 6.0
 num_points = 60
 
-M_values = [1, 4, 16, 64, 256]
-# M_values = [1]
+# M_values = [1, 4, 16, 64, 256]
+M_values = [1, 4, 16, 64]
 for M in M_values:
-    print(M)
+    for iter in range(n_models):
+        print(M)
 
-    if M > 1:
-        step_size = float(num_epochs - num_epochs_low)/float(M-1)
-    else:
-        step_size = 0
-    print(step_size)
+        if M > 1:
+            step_size = float(num_epochs - num_epochs_low)/float(M-1)
+        else:
+            step_size = 0
+        print(step_size)
 
-    networks = []
-    for i in range(M):
-        print (int(num_epochs - i*step_size))
+        networks = []
+        for i in range(M):
+            print (int(num_epochs - i*step_size))
 
-        network = ToyNet("eval_" + optimizer_name, project_dir=base_dir).cuda()
-        step = str(int(num_epochs - i*step_size))
-        checkpoint_path = base_dir + f"/training_logs/model_{optimizer_name}-1/checkpoints/model_{optimizer_name}-1_epoch_{step}.pth"
-        network.load_state_dict(torch.load(checkpoint_path))
-        networks.append(network)
+            network = ToyNet(f"eval_{model_id}-{num_epochs}_1-{n_models}", project_dir=base_dir).cuda()
+            step = str(int(num_epochs - i*step_size))
+            checkpoint_path = base_dir + f"/training_logs/model_{model_id}_{iter+1}/checkpoints/model_{model_id}_{iter+1}_epoch_{step}.pth"
+            network.load_state_dict(torch.load(checkpoint_path))
+            networks.append(network)
 
-    M_float = float(len(networks))
-    print (M_float)
+        M_float = float(len(networks))
+        print (M_float)
 
-    for network in networks:
-        network.eval()
+        for network in networks:
+            network.eval()
 
-    false_prob_values = np.zeros((num_points, num_points))
-    x_values = np.linspace(x_min, x_max, num_points, dtype=np.float32)
-    for x_1_i, x_1_value in enumerate(x_values):
-        for x_2_i, x_2_value in enumerate(x_values):
-            x = torch.from_numpy(np.array([x_1_value, x_2_value])).unsqueeze(0).cuda() # (shape: (1, 2))
+        false_prob_values = np.zeros((num_points, num_points))
+        x_values = np.linspace(x_min, x_max, num_points, dtype=np.float32)
+        for x_1_i, x_1_value in enumerate(x_values):
+            for x_2_i, x_2_value in enumerate(x_values):
+                x = torch.from_numpy(np.array([x_1_value, x_2_value])).unsqueeze(0).cuda() # (shape: (1, 2))
 
-            mean_prob_vector = np.zeros((2, ))
-            for network in networks:
-                logits = network(x) # (shape: (1, num_classes)) (num_classes==2)
-                prob_vector = F.softmax(logits, dim=1) # (shape: (1, num_classes))
+                mean_prob_vector = np.zeros((2, ))
+                for network in networks:
+                    logits = network(x) # (shape: (1, num_classes)) (num_classes==2)
+                    prob_vector = F.softmax(logits, dim=1) # (shape: (1, num_classes))
 
-                prob_vector = prob_vector.data.cpu().numpy()[0] # (shape: (2, ))
+                    prob_vector = prob_vector.data.cpu().numpy()[0] # (shape: (2, ))
 
-                mean_prob_vector += prob_vector/M_float
+                    mean_prob_vector += prob_vector/M_float
 
-            false_prob_values[x_2_i, x_1_i] = mean_prob_vector[0]
+                false_prob_values[x_2_i, x_1_i] = mean_prob_vector[0]
 
     plt.figure(1)
     x_1, x_2 = np.meshgrid(x_values, x_values)
     plt.pcolormesh(x_1, x_2, false_prob_values, cmap="RdBu", vmin=0, vmax=1)
     plt.colorbar()
     plt.tight_layout(pad=0.1, w_pad=0.1, h_pad=0.1)
-    plt.savefig("%s/predictive_density_M=%d_%d.png" % (network.model_dir, M, 1))
+    plt.savefig("%s/predictive_density_M=%d_%d.png" % (network.model_dir, M, iter+1))
     plt.close(1)
--- train_prob_distrib.py
+++ train_prob_distrib.py
@@ -1,7 +1,10 @@
+# adapted from: https://github.com/fregu856/evaluating_bdl/blob/master/toyClassification/SGLD-64/train.py
+
 import torch
 import torch.nn.functional as F
 from lib.dataset import ToyDataset
 from lib.model import ToyNet
+import lib.lr_setter as lr_setter
 import lib.psgld2 as psgld2
 import lib.sgld3 as sgld3
 import lib.ekfac_precond as ekfac
@@ -54,6 +57,7 @@ with open(yaml_path) as f:
     config = yaml.load(f, Loader=yaml.Loader)
 
 seed = config['seed']
+M = config['M']
 epochs = config['epoch']
 block_size = config['block_size']
 block_decay = config['block_decay']
@@ -61,14 +65,7 @@ optimizer_name = config['optimizer']
 precond_name = config['preconditioner']
 
 batch_size = 32
-
-model_id = optimizer_name + '-1'
-model = ToyNet(model_id, project_dir=project_dir).cuda()
-train_dataset = ToyDataset(dataset_dir)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-
-num_train_batches = int(len(train_dataset)/batch_size)
-num_steps = epochs*num_train_batches + 1
+optimizer_id = optimizer_name.split('.')[1]
 
 optim_params = {}
 if optimizer_name in config:
@@ -78,17 +75,6 @@ if optimizer_name in config:
         if v or v == False:
             optim_params[k] = v
 
-print('optimizer: ', optimizer_name)
-print('optimizer params: ', optim_params)
-if config['accept_model']:
-    optimizer = eval(optimizer_name)(model, **optim_params)
-else:
-    optimizer = eval(optimizer_name)(model.parameters(), **optim_params)
-
-# check if optimizer.step has 'lr' param
-step_args = inspect.signature(optimizer.step)
-lr_param = 'lr' in step_args.parameters
-
 precond_params = {}
 precond = None
 if precond_name in config:
@@ -97,53 +83,83 @@ if precond_name in config:
         v = precond_params2[k]
         if v or v == False:
             precond_params[k] = v
-if precond_name != '' and precond_name.lower() != 'none':
-    precond = eval(precond_name)(model, **precond_params)
-    print('preconditioner: ', precond_name)
-    print('preconditioner params: ', precond_params)
 
 if logdir != default_none:
     writer = SummaryWriter(log_dir=logdir)
 else:
     writer = SummaryWriter()
 
-step = 0
-current_lr = optim_params["lr"]
+for i in range(M):
+    model_id = f"{optimizer_id}-{epochs}_{i+1}"
+    model = ToyNet(model_id, project_dir=project_dir).cuda()
+    train_dataset = ToyDataset(dataset_dir)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-epoch_losses_train = []
-# for epoch in trange(epochs, desc='epoch', position=1, leave=False):
-for epoch in range(epochs):
-    model.train() # (set in training mode, this affects BatchNorm and dropout)
-    batch_losses = []
+    num_train_batches = int(len(train_dataset)/batch_size)
+    num_steps = epochs*num_train_batches + 1
+    num_epochs_low = int(0.75*epochs)
 
-    t0 = time.time()
-    for data, target in train_loader:
-        data = data.cuda()
-        target = target.cuda()
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        
-        loss_value = loss.data.cpu().numpy()
-        batch_losses.append(loss_value)
+    # print('optimizer: ', optimizer_name)
+    # print('optimizer params: ', optim_params)
+    if config['accept_model']:
+        optimizer = eval(optimizer_name)(model, **optim_params)
+    else:
+        optimizer = eval(optimizer_name)(model.parameters(), **optim_params)
 
-        loss.backward()
-        if precond:
-            precond.step()
-        if block_size > 0 and block_decay > 0 and lr_param:
-            optimizer.step(lr=current_lr)
-        else:
-            optimizer.step()
+    # check if optimizer.step has 'lr' param
+    step_args = inspect.signature(optimizer.step)
+    lr_param = 'lr' in step_args.parameters
 
-    elapsed = time.time() - t0
-    epoch_loss = np.mean(batch_losses)
-    epoch_losses_train.append(epoch_loss)
 
-    writer.add_scalar("Loss/train", epoch_loss, epoch)
-    writer.add_scalar("Duration", elapsed, epoch)
+    if precond_name != '' and precond_name.lower() != 'none':
+        precond = eval(precond_name)(model, **precond_params)
+        # print('preconditioner: ', precond_name)
+        # print('preconditioner params: ', precond_params)
 
-    # save the model weights to disk:
-    checkpoint_path = model.checkpoints_dir + "/model_" + model_id +"_epoch_" + str(epoch+1) + ".pth"
-    torch.save(model.state_dict(), checkpoint_path)
+    step = 0
+    current_lr = optim_params["lr"]
 
-writer.flush()
+    epoch_losses_train = []
+    for epoch in range(epochs):
+        model.train() # (set in training mode, this affects BatchNorm and dropout)
+        batch_losses = []
+
+        t0 = time.time()
+        for data, target in train_loader:
+            data = data.cuda()
+            target = target.cuda()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.cross_entropy(output, target)
+            
+            loss_value = loss.data.cpu().numpy()
+            batch_losses.append(loss_value)
+
+            loss.backward()
+            if precond:
+                precond.step()
+            if block_size > 0 and block_decay > 0 and lr_param:
+                optimizer.step(lr=current_lr)
+            else:
+                optimizer.step()
+
+        elapsed = time.time() - t0
+        epoch_loss = np.mean(batch_losses)
+        epoch_losses_train.append(epoch_loss)
+
+        # update learning rate for next epoch
+        if block_size > 0 and block_decay > 0 and ((epoch) % block_size) == 0:
+            current_lr = current_lr * block_decay
+            if not lr_param:
+                optimizer = lr_setter.update_lr(optimizer, current_lr)
+
+        writer.add_scalar("Loss/train", epoch_loss, epoch*(i+1))
+        writer.add_scalar("Duration", elapsed, epoch*(i+1))
+
+        # save the model weights to disk:
+        # only save last 75% of the epoch:
+        if epoch >= num_epochs_low:
+            checkpoint_path = model.checkpoints_dir + "/model_" + model_id +"_epoch_" + str(epoch+1) + ".pth"
+            torch.save(model.state_dict(), checkpoint_path)
+
+    writer.flush()
