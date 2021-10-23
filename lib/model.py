2bf7f3627470bcd0357013f0436f5c71857f5c62
hanif <dev.har07@gmail.com>
2021-10-23 17:04:37 +0700

    configurable model for MNIST training (4/n)

--- lib/model.py
+++ lib/model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from skimage import io, transform
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import glob
@@ -98,22 +99,25 @@ class LeNetMnist(nn.Module):
     def __init__(self, output_logits=False):
         super(LeNetMnist, self).__init__()
         self.output_logits = output_logits
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.fc1 = nn.Linear(16*5*5, 120)
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(x.size(0), -1)
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square, you can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        
+
         if self.output_logits:
             return x
         
